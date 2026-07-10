f9bfa6354277f01cf3459894794b05cf0b8179cf
litanyu <litanyu2005@outlook.com>
2026-07-10 10:23:35 +0800

    添加后端项目并更新操作日志

---
+++ codenow-backend/src/main/java/com/codenow/CodenowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodenowBackendApplication.class, args);
+    }
+
+}
---
+++ codenow-backend/src/test/java/com/codenow/CodenowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
